30d2e276207c4b80638c5f1b3eb2660d675a1c2f
uditdev1 <uditdev0008@gmail.com>
2026-03-31 00:15:54 +0530

    config: enable dark mode class strategy in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
-module.exports = {
-    darkMode: 'class',
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };

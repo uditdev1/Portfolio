ae78b807bb41cf89c3857211bb8ca18d986e9535
uditdev1 <uditdev0008@gmail.com>
2026-04-02 09:20:05 +0530

    feat: add dark mode bg and border classes to BorderWrapper

--- src/components/BorderWrapper.tsx
+++ src/components/BorderWrapper.tsx
@@ -1,11 +1,10 @@
 const BorderWrapper = ({ containerClass, boxClass, children }: any) => {
-
     return (
         <div
-            className={`font-shd w-screen px-[4%] sm:px-[8%] md:px-[15%] border-t-[1px] border-neutral-200 border-b-[1px]` + ` ${containerClass || ''}`}
+            className={`font-shd w-screen px-[4%] sm:px-[8%] md:px-[15%] border-t-[1px] border-neutral-200 dark:border-neutral-800 border-b-[1px] bg-white dark:bg-gray-950 transition-colors duration-300` + ` ${containerClass || ''}`}
         >
             <div
-                className={`text-[15px] font-[500] border-l-[1px] border-r-[1px] border-neutral-200 ` + ` ${boxClass || ''}`}
+                className={`text-[15px] font-[500] border-l-[1px] border-r-[1px] border-neutral-200 dark:border-neutral-800 dark:text-gray-100 transition-colors duration-300` + ` ${boxClass || ''}`}
             >
                 {children}
             </div>

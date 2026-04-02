b7fea49f5b4bfbed8ef49182dfd404755bb3a6cf
uditdev1 <uditdev0008@gmail.com>
2026-04-02 09:21:00 +0530

    feat: add dark mode text classes to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,14 @@ const Footer = () => {
     };
 
     return (
-        <BorderWrapper boxClass='flex flex-col justify-center items-center text-center py-[12px] text-zinc-500'>
-            <span className="text-zinc-400">Inspired by ncdai</span>
-            <span onClick={handleLinkedIn} className="cursor-pointer hover:underline text-zinc-500">
+        <BorderWrapper boxClass='flex flex-col justify-center items-center text-center py-[12px] transition-colors duration-300'>
+            <span className="text-zinc-400 dark:text-zinc-500">
+                Inspired by ncdai
+            </span>
+            <span
+                onClick={handleLinkedIn}
+                className="cursor-pointer hover:underline text-zinc-500 dark:text-zinc-400 transition-colors duration-300"
+            >
                 Built by uditdev
             </span>
         </BorderWrapper>

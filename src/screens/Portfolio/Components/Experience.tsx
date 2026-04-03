1f955d0c53fd2436624f4ac4b0877340b38bffeb
uditdev1 <uditdev0008@gmail.com>
2026-04-03 18:32:53 +0530

    feat: add dark mode classes to Experience component

--- src/screens/Portfolio/Components/Experience.tsx
+++ src/screens/Portfolio/Components/Experience.tsx
@@ -1,27 +1,18 @@
 import BorderWrapper from '../../../components/BorderWrapper'
-import { education_roles,
-     freelance_roles,
-     // wizcommerce_roles
-     } from './utils'
+import { education_roles, freelance_roles } from './utils'
 import ExperienceBox from './ExperienceBox'
 
 const Experience = () => {
-
     return (
         <>
-            <BorderWrapper boxClass="text-[36px] px-[12px] h-fit leading-11 font-[500]">
+            <BorderWrapper boxClass="text-[36px] px-[12px] h-fit leading-11 font-[500] text-zinc-950 dark:text-white transition-colors duration-300">
                 Experience
             </BorderWrapper>
-            {/* <ExperienceBox
-                company={'WizCommerce'}
-                companyIcon={<img src='/Logo/wizcommerce_logo.jpg' className='h-[24px] w-[24px] ' />}
-                roles={wizcommerce_roles}
-            /> */}
             <ExperienceBox
                 company={'Freelance'}
                 companyIcon={
                     <div className='h-[24px] w-[24px] flex justify-center items-center'>
-                        <div className='bg-zinc-200 h-[12px] w-[12px] rounded-full' />
+                        <div className='bg-zinc-200 dark:bg-zinc-600 h-[12px] w-[12px] rounded-full transition-colors duration-300' />
                     </div>
                 }
                 roles={freelance_roles}
@@ -30,7 +21,7 @@ const Experience = () => {
                 company={'Education'}
                 companyIcon={
                     <div className='h-[24px] w-[24px] flex justify-center items-center'>
-                        <div className='bg-zinc-200 h-[12px] w-[12px] rounded-full' />
+                        <div className='bg-zinc-200 dark:bg-zinc-600 h-[12px] w-[12px] rounded-full transition-colors duration-300' />
                     </div>
                 }
                 roles={education_roles}

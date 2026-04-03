d1e03ae7a1eed737b4542c90effc6373dba311fe
uditdev1 <uditdev0008@gmail.com>
2026-04-03 18:38:30 +0530

    feat: add dark mode classes to Profile component

--- src/screens/Portfolio/Components/Profile.tsx
+++ src/screens/Portfolio/Components/Profile.tsx
@@ -1,31 +1,28 @@
 import BorderWrapper from "../../../components/BorderWrapper";
-// import RedCarModel from "./RedCarModel";
 
 const Profile = () => {
-
     return (
         <BorderWrapper>
-            <div className="w-full flex row ">
-                <div className="h-[160px] relative min-w-[160px] border-1 bg-gradient-to-t from-zinc-300 to-zinc-100 border-neutral-200 rounded-full overflow-hidden">
-                    <img 
+            <div className="w-full flex row">
+                <div className="h-[160px] relative min-w-[160px] border-1 bg-gradient-to-t from-zinc-300 to-zinc-100 dark:from-zinc-700 dark:to-zinc-800 border-neutral-200 dark:border-neutral-700 rounded-full overflow-hidden transition-colors duration-300">
+                    <img
                         className="h-[80%] w-[80%] absolute left-1/2 -translate-x-1/2 bottom-0 object-cover"
-                        src="/logo.webp"  
-                        alt="" 
+                        src="/logo.webp"
+                        alt=""
                     />
                 </div>
-                <div className="border-l-1 border-neutral-200 min-h-full w-full flex flex-col justify-end">
-                    <div className=" px-[12px] relative text-[12px] text-zinc-300">
+                <div className="border-l-1 border-neutral-200 dark:border-neutral-800 min-h-full w-full flex flex-col justify-end transition-colors duration-300">
+                    <div className="px-[12px] relative text-[12px] text-zinc-300 dark:text-zinc-600">
                         <span>
                             text-3xl &nbsp; text-zinc-950 &nbsp; font-f2
                         </span>
-                        <div className="absolute -bottom-[26px] right-[12px] h-[80px] w-[250px] ">
-                            {/* <RedCarModel /> */}
+                        <div className="absolute -bottom-[26px] right-[12px] h-[80px] w-[250px]">
                         </div>
                     </div>
-                    <div className="border-t-1 max-md:leading-10 text-zinc-950 font-f2 font-[600] px-[12px] text-[32px] border-neutral-200 min-w-full">
+                    <div className="border-t-1 max-md:leading-10 text-zinc-950 dark:text-white font-f2 font-[600] px-[12px] text-[32px] border-neutral-200 dark:border-neutral-800 min-w-full transition-colors duration-300">
                         Udit Dev
                     </div>
-                    <div className="border-t-1 whitespace-nowrap border-neutral-200 w-full px-[12px] text-[18px] py-[4px] text-zinc-500">
+                    <div className="border-t-1 whitespace-nowrap border-neutral-200 dark:border-neutral-800 w-full px-[12px] text-[18px] py-[4px] text-zinc-500 dark:text-zinc-400 transition-colors duration-300">
                         Full Stack Developer
                     </div>
                 </div>

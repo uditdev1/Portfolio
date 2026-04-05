2a83062b374c9e75a78c6fe29ea09b0affcece5c
uditdev1 <uditdev0008@gmail.com>
2026-04-05 23:28:50 +0530

    refactor:blockchain

--- src/components/ScreenRoutes.tsx
+++ src/components/ScreenRoutes.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Portfolio from '../screens/Portfolio/Portfolio'
 import Components from '../screens/UiComponents/Components'
 import ComponentDetails from '../screens/ComponentDetails/ComponentDetails'
-import BlockchainProjects from '../screens/Blockchain/BlockchainProjects'
+// import BlockchainProjects from '../screens/Blockchain/BlockchainProjects'
 import Designs from '../screens/Designs/Designs'
 
 const ScreenRoutes = () => {
@@ -13,7 +13,7 @@ const ScreenRoutes = () => {
             <Route path="/components" element={<Components />} />
             <Route path="/components/:counter" element={<ComponentDetails />} />
             <Route path="/designs" element={<Designs />} />
-            <Route path="/blockchain" element={<BlockchainProjects />} />
+            {/* <Route path="/blockchain" element={<BlockchainProjects />} /> */}
         </Routes>
     )
 }
